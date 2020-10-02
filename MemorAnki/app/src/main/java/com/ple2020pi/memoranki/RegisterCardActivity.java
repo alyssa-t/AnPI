963b86d9900a802aa76553d3417a1d0e4e7970f1
Matheus Lima <m.mathelima@gmail.com>
2020-10-02 20:33:31 -0300

    Criada projeto MemorAnki
    Juntei os dois projetos (android-test e
    Kuromoji_test ) em um único projeto de android studio. Os dois estão funcionando juntos, agr é só ir montando as coisas aqui
    
    Adicionado .gitignore de android studio
    
    Co-Authored-By: Alyssa <alyssa-t@users.noreply.github.com>

---
+++ MemorAnki/app/src/main/java/com/ple2020pi/memoranki/RegisterCardActivity.java
@@ -0,0 +1,190 @@
+package com.ple2020pi.memoranki;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.atilika.kuromoji.TokenizerBase;
+import com.atilika.kuromoji.ipadic.Token;
+import com.atilika.kuromoji.ipadic.Tokenizer;
+
+import java.util.List;
+
+public class RegisterCardActivity extends AppCompatActivity {
+
+    Tokenizer tokenizer = new Tokenizer.Builder().mode(TokenizerBase.Mode.NORMAL).build();
+
+    private OpenHelper helper;
+    String kbn = "";
+    String toastMessage_added = "Adicionado com sucesso";
+    String toastMessage_mod = "Atualizado com sucesso";
+    String toastMessage_failed = "Preencha o nome do grupo";
+
+    //comit
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_card);
+
+        helper = new OpenHelper(getApplicationContext());
+        Intent intent = getIntent();
+
+        String KBN = intent.getStringExtra("KBN");
+        Button btn_confirmGroupName = findViewById(R.id.btn_confirmGroupName);
+        Button btn_delete = findViewById(R.id.btn_delete);
+
+        if(KBN.length() != 0){
+            kbn = KBN;
+            btn_confirmGroupName.setText(R.string.atualizar);
+            btn_delete.setVisibility(View.VISIBLE);
+            readDate(KBN);
+
+        }
+        else{
+            kbn = "add";
+            btn_confirmGroupName.setText(R.string.adicionar);
+        }
+
+        EditText editText = findViewById(R.id.txt_inputGroupName);
+        final TextView text = findViewById(R.id.textInfo);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                text.setText("");
+                List<Token> tokens = tokenizer.tokenize(s.toString());
+
+                for (int i = 0; i < tokens.size(); i++) {
+                    String display = tokens.get(i).getBaseForm() + "(" + tokens.get(i).getReading() + ") :" + tokens.get(i).getPartOfSpeechLevel1() + "\n";
+                    text.append(display);
+                }
+            }
+        });
+    }
+
+    public void SaveGroupData(View view) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        EditText txtGroupName = findViewById(R.id.txt_inputGroupName);
+
+        String groupName = txtGroupName.getText().toString();
+
+        ContentValues values = new ContentValues();
+        values.put("groupName", groupName);
+
+        if (kbn == "add"){
+            if (groupName.length() != 0){
+                db.insert("mycardtb", null, values);
+                toastMake(toastMessage_added, 0 , +350);
+                txtGroupName.setText("");
+            }else{
+                toastMake(toastMessage_failed, 0, +350);
+            }
+        }
+        //atualizarr
+        else{
+            if (groupName.length() != 0){
+                UPData(kbn);
+                toastMake(toastMessage_mod, 0 , +350);
+                finish();
+            }else{
+                toastMake(toastMessage_failed, 0, +350);
+            }
+
+        }
+    }
+    private void toastMake(String message, int x, int y) {
+        Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, x, y);
+        toast.show();
+    }
+
+    public void readDate(String read){
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        EditText text1 = findViewById(R.id.txt_inputGroupName);
+        Cursor cursor = db.query(
+                "mycardtb",
+                new String[]{"groupName"},
+                "groupName = ?",
+                new String[]{read},
+                null,null,null
+        );
+        cursor.moveToFirst();
+
+        for(int i = 0; i< cursor.getCount(); i++){
+            text1.setText(cursor.getString(0));
+
+        }
+
+        cursor.close();
+    }
+    public void UPData(String read){
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        EditText txt1 = findViewById(R.id.txt_inputGroupName);
+
+        String groupName = txt1.getText().toString();
+
+
+        ContentValues upvalue = new ContentValues();
+        upvalue.put("groupName",groupName);
+
+
+        db.update("mycardtb",upvalue,"groupName=?",new String[]{read});
+    }
+
+    public void DeleteCard(View view) {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("Apagar grupo");
+        builder.setMessage("Gostaria de apagar esse grupo?");
+        builder.setPositiveButton("Sim",
+                new DialogInterface.OnClickListener() {
+                    @Override
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        SQLiteDatabase db = helper.getWritableDatabase();
+                        db.delete("mycardtb", "groupName=?", new String[]{kbn});
+                        finish();
+                    }
+                });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void Return(View view) {
+        finish();
+    }
+}
---
+++ MemorAnki/app/src/main/java/com/ple2020pi/memoranki/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,153 @@
+package com.ple2020pi.memoranki.ui.notifications;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.ple2020pi.memoranki.MainActivity;
+import com.ple2020pi.memoranki.OpenHelper;
+import com.ple2020pi.memoranki.R;
+import com.ple2020pi.memoranki.RegisterCardActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class NotificationsFragment extends Fragment {
+
+    private NotificationsViewModel notificationsViewModel;
+    private boolean allowRefresh = false;
+    private android.widget.ListView myListView = null;
+    private SQLiteDatabase db;
+    private OpenHelper myOpenHelper;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        notificationsViewModel =
+                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        myOpenHelper = new OpenHelper(getActivity());
+        myListView = root.findViewById(R.id.listview_gerenciarGrupo);
+        db = myOpenHelper.getWritableDatabase();
+
+        Cursor c = db.rawQuery("select * from mycardtb", null);
+        String[] from = {"groupName"};
+        int[] to = {android.R.id.text1};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_list_item_1, c, from, to, 0);
+        myListView.setAdapter(adapter);
+        myListView.setItemsCanFocus(false);
+
+        //vigia se clicaram em um item especifico da lista
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                String s1 = ((TextView) view.findViewById(android.R.id.text1)).getText().toString();
+                //String s2  = ((TextView)view.findViewById(android.R.id.text2)).getText().toString();
+
+                Intent intent = new Intent(getActivity().getApplication(), RegisterCardActivity.class);
+                intent.putExtra("KBN", s1);
+                startActivity(intent);
+            }
+        });
+
+        //função para o botao de (+) para add grupo
+
+        myListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int pos, long id) {
+                // TODO Auto-generated method stub
+
+                toastMake("LongClickkkk", 0, 350);
+
+                return true;
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+    private void toastMake(String message, int x, int y) {
+        Toast toast = Toast.makeText(this.getActivity(), message, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, x, y);
+        toast.show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //Initialize();
+        if (allowRefresh) {
+            allowRefresh = false;
+            reload();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (!allowRefresh)
+            allowRefresh = true;
+    }
+
+    public void reload(){
+        db = myOpenHelper.getWritableDatabase();
+
+        Cursor c = db.rawQuery("select * from mycardtb", null);
+        String[] from = {"groupName"};
+        int[] to = {android.R.id.text1};
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_list_item_1, c, from, to, 0);
+        myListView.setAdapter(adapter);
+
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.option_menu_first, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.menu_first_a){
+            Intent intent = new Intent(getActivity().getApplication(), RegisterCardActivity.class);
+            intent.putExtra("KBN", "");
+            startActivity(intent);
+        }
+        else{
+            toastMake("Eu ainda sou inutil", 0, 350);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
